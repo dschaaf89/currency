35f2eb1e31ca9e8d004586c69b0cd5e8a936a4c6
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-11 16:49:44 -0700

    added some changes to the getElements functions

--- src/js/service-logic.js
+++ src/js/service-logic.js
@@ -5,13 +5,13 @@ export function getElements(response, money, currency) {
   if (type === "CAD") {
     $('#results').text(`you can convert your currency into ${response.conversion_rates.CAD * `${money}`} dollars`);
   } else if (type === "AED") {
-    $('#results').text(`you can convert your currency into ${response.conversion_rates.AED * `${money}`}`);
+    $('#results').text(`you can convert your currency into ${response.conversion_rates.AED * `${money}`} dirham`);
   } else if (type === "ARS") {
-    $('#results').text(`you can convert your currency into ${response.conversion_rates.ARS * `${money}`}`);
+    $('#results').text(`you can convert your currency into ${response.conversion_rates.ARS * `${money}`}  peso`);
   } else if (type === "AUD") {
-    $('#results').text(`you can convert your currency into ${response.conversion_rates.AUD * `${money}`}`);
+    $('#results').text(`you can convert your currency into ${response.conversion_rates.AUD * `${money}`} dollars`);
   } else {
-    $('#results').text(`you can convert your currency into ${response.conversion_rates.EUR * `${money}`}`);
+    $('#results').text(`you can convert your currency into ${response.conversion_rates.EUR * `${money}`} Euro's`);
   }
 }
 
