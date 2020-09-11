d22bfed0c5aabf670f80e1aec56b8a0389308632
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-11 09:31:02 -0700

    add the ability to take in user input for currency

--- src/main.js
+++ src/main.js
@@ -4,19 +4,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
+
 $(document).ready(function () {
+  $('#currency').submit(function () {
+    event.preventDefault();
+    let currency = $('select#typeOfCurrency').val();
+    let money = $('#money').val();
+    console.log(money,currency);
 
 
-  let request = new XMLHttpRequest();
-  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
 
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      getElements(response);
+    let request = new XMLHttpRequest();
+
+    request.open("GET", url, true);
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    function getElements(response) {
+      
+
     }
-  };
-  request.open("GET", url, true);
-  request.send();
 
+
+
+  });
 });
