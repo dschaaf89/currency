94ef6876850fc2136b704a2a1d7700a31fe97d0f
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-11 16:18:31 -0700

    fixed error

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     event.preventDefault();
     let currency =$('#typeOfCurrency').val();
     let money = $('#money').val();
-    let url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
     let request = new XMLHttpRequest();
     request.open("GET", url, true);
     request.send();
@@ -22,3 +22,26 @@ $(document).ready(function () {
     };
   });
 });
+
+
+
+// let promise = new Promise(function(resolve, reject) {
+//   let request = new XMLHttpRequest();
+//   const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+//   request.onload = function() {
+//     if (this.status === 200) {
+//       resolve(request.response);
+//     } else {
+//       reject(request.response);
+//     }
+//   };
+//   request.open("GET", url, true);
+//   request.send();
+// });
+
+// promise.then(function(response) {
+//   getElements(response,money,currency);
+// }, function(error) {
+//   $('.showErrors').text(`There was an error processing your request: ${error}`);
+  
+// });
